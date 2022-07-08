95ffe78fa7e1019e5c2fa11162f43f197f3b6368
Sbbars <sbbarse2121@gmail.com>
2022-07-08 11:07:32 +0530

    Make website responsive

--- static/js/test.js
+++ static/js/test.js
@@ -1,13 +1,13 @@
-// window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
         
-//         function scrollFunction() {
-//           if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//             document.getElementById("navbar").style.padding = "10px 10px";
-//             document.getElementById("logo").style.fontSize = "25px";
-//           } else {
-//             document.getElementById("navbar").style.padding = "50 px 10px";
-//             document.getElementById("logo").style.fontSize = "35px";
-//           }
-//         }
+        function scrollFunction() {
+          if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("navbar").style.padding = "25px 25px";
+            document.getElementById("logo").style.fontSize = "25px";
+          } else {
+            document.getElementById("navbar").style.padding = " 25px 25px";
+            document.getElementById("logo").style.fontSize = "30px";
+          }
+        }
 
        
