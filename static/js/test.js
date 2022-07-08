919e8d8cf8b99614dbc097cd9ae6dd1db3f15500
Sbbars <sbbarse2121@gmail.com>
2022-07-08 20:46:59 +0530

    update responses and view of site

--- static/js/test.js
+++ static/js/test.js
@@ -2,11 +2,11 @@ window.onscroll = function() {scrollFunction()};
         
         function scrollFunction() {
           if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-            document.getElementById("navbar").style.padding = "25px 25px";
+            document.getElementById("navbar").style.padding = "5px 5px";
             document.getElementById("logo").style.fontSize = "25px";
           } else {
-            document.getElementById("navbar").style.padding = " 25px 25px";
-            document.getElementById("logo").style.fontSize = "30px";
+            document.getElementById("navbar").style.padding = " 15px 15px";
+            document.getElementById("logo").style.fontSize = "25px";
           }
         }
 
