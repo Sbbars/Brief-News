f8b80ecee5feeadcc9b9161c471b47ec3ec77605
Sbbars <sbbarse2121@gmail.com>
2022-07-08 18:58:07 +0530

    Final response added for mobile view

--- static/js/test.js
+++ static/js/test.js
@@ -1,13 +1,13 @@
-// window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
         
-//         function scrollFunction() {
-//           if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//             document.getElementById("navbar").style.padding = "25px 25px";
-//             document.getElementById("logo").style.fontSize = "25px";
-//           } else {
-//             document.getElementById("navbar").style.padding = " 25px 25px";
-//             document.getElementById("logo").style.fontSize = "30px";
-//           }
-//         }
+        function scrollFunction() {
+          if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+            document.getElementById("navbar").style.padding = "25px 25px";
+            document.getElementById("logo").style.fontSize = "25px";
+          } else {
+            document.getElementById("navbar").style.padding = " 25px 25px";
+            document.getElementById("logo").style.fontSize = "30px";
+          }
+        }
 
        
